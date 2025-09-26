de54be5d110722160913000ff61ffaf0177b7e0e
sabari <ssgreeshan@gmail.com>
2025-09-26 08:59:12 +0530

    test3 testing

--- src/servers/testCall3.js
+++ src/servers/testCall3.js
@@ -1,33 +1,42 @@
-import express from "express";
-import mySqlDb1 from "#databases/mySqlDb1.js";
-import routeSetup from "#configs/routeSetup.js";
+import express
+	from "express";
+import mySqlDb1
+	from "#databases/mySqlDb1.js";
+import routeSetup
+	from "#configs/routeSetup.js";
 
-export default express.Router()
-	.get(routeSetup(import.meta.url),
+const { Router } = express;
+const router = Router();
+const get = router.get.bind(router);
+const { url: serverFileUrl } = import.meta;
+const serverRoute = routeSetup(serverFileUrl);
 
-		async (req, res) => {
-			try {
-				const { body } = req;
-				console.log(body);
-				// const { piece } = body;
-				const query = `
+export default get(
+	serverRoute,
+
+	async (req, res) => {
+		try {
+			const { body } = req;
+			console.log(body);
+			// const { piece } = body;
+			const query = `
 				SELECT * FROM test1 WHERE name=?;
 				`;
-				const values = [
-					// piece
-					"test A"
-				]
-				console.log(query.trim())
-				console.log(values);
-				const [result] = await mySqlDb1.query(query, values);
-				console.log(result);
+			const values = [
+				// piece
+				"test A"
+			]
+			console.log(query.trim())
+			console.log(values);
+			const [result] = await mySqlDb1.query(query, values);
+			console.log(result);
 
-				/#finale#/
-				res.status(200).json({ data: result });
-				/########/
-			} catch (error) {
-				console.error(error)
-				res.status(500).json({ error })
-			}
+			/#finale#/
+			res.status(200).json({ data: result });
+			/########/
+		} catch (error) {
+			console.error(error)
+			res.status(500).json({ error })
 		}
-	)
+	}
+)
